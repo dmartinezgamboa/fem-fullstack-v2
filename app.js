dca013998e789f83877ada09f264b5c6ef37ff00
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2021-09-16 11:21:31 -0700

    /custom route was not working correctly, will test locally until it is working

--- app.js
+++ app.js
@@ -9,10 +9,4 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/custom"),
-  (req, res) => {
-    res.status(418);
-    res.send({ status: "ok", message: "Does it show a custom status?" });
-  };
-
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
